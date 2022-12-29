ae028ce99a68dc844ee4cfd616f545a564a17921
gaston964 <gastongutierrez964@gmail.com>
2022-12-29 20:38:13 -0300

    FireBase 2.3 n CSS

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -8,7 +8,7 @@ import { CartContext } from '../context/CartContext'
 const Cart = ({ condition }) => {
     const { cart, removeItem, emptyCart, getTotalPrice } = useContext(CartContext)
     condition = cart.length === 0
-    
+
     return (
         <div className='cartview'>
             {condition &&
@@ -20,28 +20,30 @@ const Cart = ({ condition }) => {
                 </div>
             }
             {!condition &&
-                <div className='cartPadre'>
+                <>
                     <h1>Carrito de Compras</h1>
-                    <div className='cartContainer'>
-                        {cart.map(cartItm => (
-                            <div className='itemCard' key={cartItm.id}>
-                                <img className='itemcard_img' src={cartItm.pictureUrl} alt={cartItm.title} />
-                                <h1>{cartItm.title}</h1>
-                                <h3>${cartItm.price}</h3>
-                                <h4>Cantidad: {cartItm.cantidad}</h4>
-                                <h4>Subtotal: ${cartItm.cantidad * cartItm.price} </h4>
-                                <button className='item-det-button' onClick={() => removeItem(cartItm.id)} >Eliminar</button>
-                            </div>
-                        ))}
+                    <div className='cartPadre'>
+                        <div className='cartContainer'>
+                            {cart.map(cartItm => (
+                                <div className='itemCard' key={cartItm.id}>
+                                    <img className='itemcard_img' src={cartItm.pictureUrl} alt={cartItm.title} />
+                                    <h1>{cartItm.title}</h1>
+                                    <h3>${cartItm.price}</h3>
+                                    <h4>Cantidad: {cartItm.cantidad}</h4>
+                                    <h4>Subtotal: ${cartItm.cantidad * cartItm.price} </h4>
+                                    <button className='item-det-button' onClick={() => removeItem(cartItm.id)} >Eliminar</button>
+                                </div>
+                            ))}
+                        </div>
+                        <div className='cartBoE'>
+                            <h3>Total: ${getTotalPrice()}</h3>
+                            <button className='item-det-button' onClick={emptyCart}>Vaciar Carrito</button>
+                            <Link to={"/form"} >
+                                <button className={`item-det-button`} >Comprar</button>
+                            </Link>
+                        </div>
                     </div>
-                    <div className='cartBoE'>
-                        <h3>Total: ${getTotalPrice()}</h3>
-                        <button className='item-det-button' onClick={emptyCart}>Vaciar Carrito</button>
-                        <Link to={"/form"} >
-                        <button className={`item-det-button`} >Comprar</button>
-                        </Link>
-                    </div>
-                </div>
+                </>
             }
         </div>
     )
--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,12 +1,86 @@
 import { addDoc, collection, getFirestore, serverTimestamp } from 'firebase/firestore'
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
+import { CartContext } from '../context/CartContext'
+
+
+const MyForm = () => {
+    const { cart, getTotalPrice, emptyCart } = useContext(CartContext)
+    const [user, setUser] = useState({})
+    const [orderId, setOrderId] = useState('')
+    const datosComprador = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value
+        })
+    }
+    const makeOrder = async (e) => {
+        e.preventDefault()
+        let order = {
+            buyer: user,
+            items: cart,
+            total: getTotalPrice(),
+            date: serverTimestamp()
+        }
+
+        const db = getFirestore();
+        const orderCollection = collection(db, "orders")
+        const res = await addDoc(orderCollection, order)
+        setOrderId(res.id)
+        emptyCart()
+    }
+    console.log(cart)
+    return (
+        <>
+            <div className='formularioPadre' onSubmit={makeOrder}>
+                {orderId ? <h2>Muchas gracias por su compra su número de orden es: {orderId}</h2>
+                    : <form className='formulario'>
+                        <h2>Formulario de compra!</h2>
+                        <input onChange={datosComprador} type="text" name="name" placeholder="Nombre" />
+                        <input onChange={datosComprador} type="tel" name="phone" placeholder="Número de celular" />
+                        <input onChange={datosComprador} type="email" name="email" placeholder="Correo electrónico" />
+                        <button type="submit">Generar orden</button>
+                    </form>}
+                <div className='cartFormularioP'>
+                    {cart.map(cartOrder => {
+                        return (
+                            <div className='cartFormulario'>
+                                <h2>{cartOrder.title}</h2>
+                                <h3>${cartOrder.price}</h3>
+                                <h4>Cantidad: {cartOrder.cantidad}</h4>
+                                <h4>Subtotal: ${cartOrder.cantidad * cartOrder.price} </h4>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default MyForm
+
+
+
+
+
+
+/* import { addDoc, collection, getFirestore, serverTimestamp } from 'firebase/firestore'
+import React, { useContext, useState } from 'react'
 import { CartContext } from '../context/CartContext'
 
 
 const MyForm = () => {
     const { cart, getTotalPrice } = useContext(CartContext)
+    const [user, setUser] = useState({})
+    const datosComprador = (e) => {
+        setUser({
+            ...user,
+            [e.target.name]: e.target.value,
+            [e.target.phone]: e.target.value,
+            [e.target.email]: e.target.value
+        })
+    }
     const makeOrder = () => {
-        const user = { name: 'usuario', phone: '261455289', emial: 'usuario@gmail.com', }
         const order = {
             buyer: user,
             items: [{ id, title, price }],
@@ -14,13 +88,10 @@ const MyForm = () => {
             date: serverTimestamp()
         }
         console.log('Levantando la order', order);
-        saveOrder(order)
-    }
-    const saveOrder = async (order) => {
-        const db = getFirestore();
-        const orderCollection = collection(db, "orders")
-        const { id } = await addDoc(orderCollection, order)
-        console.log("Nueva Orden: ", id);
+            const db = getFirestore();
+            const orderCollection = collection(db, "orders")
+            const { id } = addDoc(orderCollection, order)
+            console.log("Nueva Orden: ", id);
     }
 
     return (
@@ -28,9 +99,9 @@ const MyForm = () => {
             <div className='formularioPadre'>
                 <form className='formulario'>
                     <h2>Formulario de compra!</h2>
-                    <input type="text" name="name" placeholder="Nombre" />
-                    <input type="tel" name="phone" placeholder="Número de celular" />
-                    <input type="email" name="email" placeholder="Correo electrónico" />
+                    <input type="text" name="name" placeholder="Nombre" onChange={datosComprador} />
+                    <input type="tel" name="phone" placeholder="Número de celular" onChange={datosComprador} />
+                    <input type="email" name="email" placeholder="Correo electrónico" onChange={datosComprador} />
                     <button type="submit">Generar orden</button>
                 </form>
                 <div className='cartFormularioP'>
@@ -50,3 +121,4 @@ const MyForm = () => {
 
 export default MyForm
 
+ */
